a00f3f05e4069bd4a82e083470124619e585806f
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-30 11:34:09 +0200

    lacal & export module

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const addFn = require("./add.js");
+// Name of the constant can be anything. Doesn't have to be thhe export value.
+
 console.log("Hello from index.js");
+
+const sum = addFn(2, 3);
+console.log(sum);
+const sum2 = addFn(3, 6);
+console.log(sum2);
