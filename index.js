cb5064793e59a1f93d72a026c4d349bbc8d4b485
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-30 14:08:33 +0200

    require

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const addFn = require("./add.js");
-// Name of the constant can be anything. Doesn't have to be thhe export value.
+require("./batman.js");
+require("./superman.js");
 
-console.log("Hello from index.js");
+// const addFn = require("./add.js");
+// // Name of the constant can be anything. Doesn't have to be thhe export value.
 
-const sum = addFn(2, 3);
-console.log(sum);
-const sum2 = addFn(3, 6);
-console.log(sum2);
+// console.log("Hello from index.js");
+
+// const sum = addFn(2, 3);
+// console.log(sum);
+// const sum2 = addFn(3, 6);
+// console.log(sum2);
