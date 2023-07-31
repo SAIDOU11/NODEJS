4e460aaaffce2874e3570554b4464a38c76d6b06
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-31 14:53:45 +0200

    module scope, wrapper & caching

---
+++ iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expression (IIFE) in Node js
+
+(function (message) {
+  const superHero = "Batman";
+  console.log(message, superHero);
+})("Hey");
+
+(function (message) {
+  const superHero = "Superman";
+  console.log(message, superHero);
+})("Hello");
