06ec6a24cc056a07ab66a39218ced489bbfd53cd
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-06 12:30:26 +0200

    http routes

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const http = require('node:http');
 const fs = require('node:fs');
+const { log } = require('node:console');
 
 const server = http.createServer((req, res) => {
+  // req.method GET POST PUT DELETE
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Home Page');
+    res.end('Home page!');
   } else if (req.url === '/about') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('About Page');
+    res.end('About page!');
   } else if (req.url === '/api') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(
-      JSON.stringify({
-        firstName: 'Bruce',
-        lastName: 'Wayne',
-      })
-    );
+    res.end('Api page!');
   } else {
     res.writeHead(404);
-    res.end('Page Not Found');
+    res.end('Page Not Found!');
   }
 });
 
